97b30ca4a62bcca9b6815d89ada3e20745ca956b
Pogogo007 <Francisd246@hotmail.com>
2022-08-25 00:17:47 -0400

    Burh

--- script.py
+++ script.py
@@ -1,26 +1,48 @@
 from asyncio import constants
 from genericpath import exists
-from importlib.resources import path
-import json
+from pathlib import Path
 import pprint as pp
+import shutil
 import subprocess
 import sys
 import os
+from tempfile import mkdtemp, tempdir
+import winreg
 from pyunpack import Archive
+import winreg
+
+regPath = winreg.QueryValueEx(winreg.OpenKeyEx(winreg.HKEY_LOCAL_MACHINE,
+                                               "SOFTWARE\WOW6432Node\Bethesda Softworks\Skyrim Special Edition"), "installed path"
+                              )
+skyrimPath = regPath[0]
+temp = mkdtemp()
+tempdata = temp + "\\data"
+os.mkdir(tempdata)
 xEditPath = "Tools\\SSEEdit\\SSEEdit.exe"
 poserDataGenPath = "Tools\\PoserDataGen\\PoserDataGen.exe"
+requirements = ["Skyrim.esm", "Update.esm"]
+
 
 def initialize():
-    if not(os.path.exists("Tools\\SSEEdit")):
-        Archive("Tools\\SSEEDIT.rar").extractall("Tools\\")
-    if not(os.path.exists("Tools\\PoserDataGen")):
+    # os.rmdir(temp)
+    # if not (os.path.exists("Tools\\SSEEdit")):
+    #    Archive("Tools\\SSEEDIT.rar").extractall("Tools\\")
+    if not (os.path.exists("Tools\\PoserDataGen")):
         Archive("Tools\\PoserDataGen.rar").extractall("Tools\\")
+    for requirement in requirements:
+        shutil.copy(skyrimPath + "data\\" + requirement, tempdata)
+    files = Path("packs\\").glob("*")
+    for file in files:
+        Archive(file).extractall(tempdata)
 
-def loadJson():
-    packsjson = json.load(open("packs.json", "r"))
-    packs = packsjson["packs"]
-    pp.pprint(packs)
-    pp.pprint(type(packs))
+    bruh = Path(tempdata).rglob('*.esp')
+    for file in bruh:
+        print(type(file))
+        cut = Path(file).parent
+        pp.pprint(cut)
+
 
 initialize()
-subprocess.call(xEditPath)
+
+# Archive(file).extractall("tools\\")
+# subprocess.call(xEditPath)
